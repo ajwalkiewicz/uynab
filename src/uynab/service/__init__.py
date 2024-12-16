62a4793e9ef80cf875f53b1cfdee0e4349540a51
Adam Walkiewicz <aj.walkiewicz@gmail.com>
2024-12-16 20:18:58 +0100

    Add __all__ in service.__init__.py to avoid to declare public interface
    * It also fix the ruff failing check for unused-import (F401)

--- src/uynab/service/__init__.py
+++ src/uynab/service/__init__.py
@@ -3,3 +3,11 @@ from .category import CategoryService
 from .payee import PayeeService
 from .service import YNABService
 from .transaction import TransactionService
+
+__all__ = [
+    "BudgetService",
+    "CategoryService",
+    "PayeeService",
+    "TransactionService",
+    "YNABService",
+]
