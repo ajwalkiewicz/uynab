7da0e95f614f1d979c4aa03ae5cb2fdea2a96aea
Adam Walkiewicz <aj.walkiewicz@gmail.com>
2025-01-04 05:21:50 +0100

    Rebase to master

--- tests/test_service_category.py
+++ tests/test_service_category.py
@@ -41,3 +41,37 @@ def test_get_category(category_service, mock_client, category_data, category_id)
     category = category_service.get_category(budget_id, category_id)
 
     assert category.id == category_id
+
+
+def test_get_all_categories_empty_groups(category_service, mock_client, category_group):
+    budget_id = UUID("12345678-1234-5678-1234-567812345678")
+    mock_client.request.return_value = {
+        "data": {
+            "category_groups": [
+                {
+                    "id": category_group.id,
+                    "name": "Empty Group",
+                    "hidden": False,
+                    "deleted": False,
+                    "categories": [],
+                }
+            ],
+            "server_knowledge": 1,
+        }
+    }
+
+    categories = category_service.get_all_categories(budget_id)
+    assert categories[0].categories == []
+
+
+def test_get_all_categories_group_with_no_categories(category_service, mock_client):
+    budget_id = UUID("12345678-1234-5678-1234-567812345678")
+    mock_client.request.return_value = {
+        "data": {
+            "category_groups": [],
+            "server_knowledge": 1,
+        }
+    }
+
+    categories = category_service.get_all_categories(budget_id)
+    assert categories == []
