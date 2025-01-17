cb930e932e05c9a190ef3953d62031def0c6f574
Adam Walkiewicz <57951750+ajwalkiewicz@users.noreply.github.com>
2025-01-17 03:31:11 +0100

    Drop support for python3.10 (#16)
    * Drop support for python3.10
    * Add FlagColor enum class instead of using Literal
    * Fix issue with tests always run on same version of python
    * Import YNABClient, BudgetService, CategoryService, PayeeService
    and TransactionService to __init__.py
    * Bump version to 0.1.2.
    * Run type_check and format_check workflows on python3.11

--- src/uynab/__init__.py
+++ src/uynab/__init__.py
@@ -2,4 +2,18 @@
 
 from importlib.metadata import version
 
+from uynab.client import YNABClient
+from uynab.service.budget import BudgetService
+from uynab.service.category import CategoryService
+from uynab.service.payee import PayeeService
+from uynab.service.transaction import TransactionService
+
 __version__ = version("uynab")
+
+__all__ = [
+    "YNABClient",
+    "BudgetService",
+    "CategoryService",
+    "PayeeService",
+    "TransactionService",
+]
